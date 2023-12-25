10d2c1c13d89af72aff339c9fb00db24266d3d11
Evgeny Sokol <eugene4ik@mail.com>
2023-12-26 01:31:58 +0700

    Step 4

--- __fixtures__/parser.js
+++ __fixtures__/parser.js
@@ -1,5 +1,7 @@
+#!/usr/bin/env node
 import { readFileSync } from 'node:fs';
 import path from 'path';
+import orderBy from 'lodash.orderBy';
 
 const resolveFilePath = (filePath) => {
   return path.isAbsolute(filePath)
@@ -19,32 +21,48 @@ const parseData = (filepath) => {
   switch (format) {
     case 'json':
       return JSON.parse(data);
-    // Добавьте обработку других форматов, если необходимо
+    // Добавить обработку других форматов кроме json
     default:
       throw new Error(`Unsupported file format: ${format}`);
   }
 };
 const gendiff = (filepath1, filepath2, options) => {
-  const absolutePath1 = resolveFilePath(filepath1); // определяем абсолютный путь к файлу
+  console.log('Текущая рабочая директория:', process.cwd());
+  // определяем абсолютный путь к файлу
+  const absolutePath1 = resolveFilePath(filepath1);
+  //console.log(absolutePath1);
   const absolutePath2 = resolveFilePath(filepath2);
-
-  const format1 = determineFormat(absolutePath1); //функция determineFormat определяет формат переданного файла
+  //функция determineFormat определяет формат переданного файла
+  const format1 = determineFormat(absolutePath1);
   const format2 = determineFormat(absolutePath2);
 
   const data1 = parseData(filepath1);
   const data2 = parseData(filepath2);
   // Результаты парса файлов
-  console.log('file 1:', data1);
-  console.log('file 2:', data2);
-  // Здесь должна быть ваша логика сравнения данных в формате format1 и format2
-  console.log(
-    `Compairinf files: ${absolutePath1} (${format1}) and ${absolutePath2} (${format2})`
-  );
 
-  // if (options.format) {
-  //   console.log(`Output format: ${options.format}`);
-  // }
-  // // Дополнительная логика обработки файлов
+  const compareFiles = (data1, data2) => {
+    const allKeys = Array.from(
+      new Set([...Object.keys(data1), ...Object.keys(data2)])
+    );
+    const sortedKeys = orderBy(allKeys);
+    const differences = {};
+
+    sortedKeys.forEach((key) => {
+      const value1 = data1[key];
+      const value2 = data2[key];
+
+      if (value1 !== value2) {
+        if (key in data1) differences[`- ${key}`] = value1;
+        if (key in data2) differences[`+ ${key}`] = value2;
+      } else {
+        if (key in data1) differences[`  ${key}`] = value1;
+      }
+    });
+
+    return differences;
+  };
+  console.log(compareFiles(data1, data2));
 };
-
 export default gendiff;
+
+// absolute path: gendiff /Users/mac/vs_code/hexlet-git/frontend-project-46/__fixtures__/file1.json /Users/mac/vs_code/hexlet-git/frontend-project-46/__fixtures__/file2.json
--- gendiff.js
+++ gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+//import compareFiles from './__fixtures__/parser.js';
 import gendiff from './__fixtures__/parser.js';
 const program = new Command();
 
