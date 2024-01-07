16869274fc259eaa8a5f88f62743f2520a6221f3
Evgeny Sokol <eugene4ik@mail.com>
2024-01-07 22:44:46 +0700

    Step 7

---  __tests__/index.test.js
+++  __tests__/index.test.js
@@ -1,18 +1,19 @@
-import gendiff from '../src/index.js';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path';
+import path, { dirname } from 'path';
+import gendiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const currentDir = dirname(filename);
 
 describe('gendiff', () => {
   it('Compare different files', () => {
     const filepath1 = './__fixtures__/file1.json';
     const filepath2 = './__fixtures__/file2.json';
 
-    const expectedFilePath = path.join(__dirname, '../__fixtures__/testForJson.txt');
+    const expectedFilePath = path.join(currentDir, '../__fixtures__/testForJson.txt');
     const expected = fs.readFileSync(expectedFilePath, 'utf8').trim();
 
     const result = gendiff(filepath1, filepath2).trim();
