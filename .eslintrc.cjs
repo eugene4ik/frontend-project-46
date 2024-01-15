0ea92151d9c203331e1cd6958b7c0d40c38e2112
Evgeny Sokol <eugene4ik@mail.com>
2024-01-15 20:06:32 +0700

    Step 9

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,14 +4,11 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: 'airbnb-base',
+  extends: ['airbnb-base'],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-  },
-  scripts: {
-    lint: 'eslint .',
   },
   rules: { 'import/extensions': ['error', 'ignorePackages'] },
 };
